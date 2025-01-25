f47daef35b98938d40456157fade34d315198235
su2do <su2do@live.com>
2025-01-25 21:31:32 +0800

    Update __init__.py

--- plugins/cleanstrm/__init__.py
+++ plugins/cleanstrm/__init__.py
@@ -146,28 +146,6 @@ class CleanStrm(_PluginBase):
     def get_api(self) -> List[Dict[str, Any]]:
         pass
 
-    def get_service(self) -> List[Dict[str, Any]]:
-        """
-        注册插件公共服务
-        [{
-            "id": "服务ID",
-            "name": "服务名称",
-            "trigger": "触发器：cron/interval/date/CronTrigger.from_crontab()",
-            "func": self.xxx,
-            "kwargs": {} # 定时器参数
-        }]
-        """
-        if self._enabled and self._cron:
-            return [
-                {
-                    "id": "CleanStrm",
-                    "name": "清理无效strm定时服务",
-                    "trigger": CronTrigger.from_crontab(self._cron),
-                    "func": self.__clean,
-                    "kwargs": {}
-                }
-            ]
-
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
         return [
             {
