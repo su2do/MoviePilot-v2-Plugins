393135a37dae56bc5c820fe88ba76d62cc637410
su2do <su2do@live.com>
2025-06-30 22:44:56 +0800

    Update __init__.py

--- plugins/cloudstrmwebdav/__init__.py
+++ plugins/cloudstrmwebdav/__init__.py
@@ -29,7 +29,7 @@ class CloudStrmwebdav(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/create.png"
     # 插件版本
-    plugin_version = "4.4.2"
+    plugin_version = "4.4.2.1"
     # 插件作者
     plugin_author = "su2do"
     # 作者主页
